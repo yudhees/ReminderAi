07b4e0e5cf995bc5f43489c2dfb3febc3547cec7
Yudhees <yudees479@gmail.com>
2026-07-05 12:43:42 +0530

    CSS update

--- app/components/ui/ChatInputBox.vue
+++ app/components/ui/ChatInputBox.vue
@@ -1,8 +1,25 @@
 <template>
-    <textarea  placeholder="Ask AI to set, edit, snooze… or tap 🎤 to speak" v-model="input"
-        class="flex-1 bg-transparent border-none outline-none text-slate-100 text-sm resize-none placeholder:text-slate-600 min-h-[22px] max-h-[120px]"
-      ></textarea>
+  <textarea placeholder="Ask AI to set, edit, snooze… or tap 🎤 to speak" v-model="input" ref="textArea"
+    class="flex-1 bg-transparent border-none outline-none text-slate-100 text-sm resize-none placeholder:text-slate-600 min-h-[22px] max-h-[120px]"></textarea>
 </template>
 <script setup>
-const input=defineModel()
+const input = defineModel();
+const textArea = ref(null);
+onMounted(() => {
+ toggleTextAreaFocus()
+});
+const toggleTextAreaFocus=()=>{
+  nextTick(() => {
+    textArea.value.focus();
+  });
+}
+onKeyStroke((e) => {
+  if (!input.value) {
+    if (/^[a-zA-Z]$/.test(e.key)) {
+      input.value=e.key
+      toggleTextAreaFocus()
+      console.log(input.value);
+    }
+  }
+});
 </script>
--- app/components/ui/NewChatText.vue
+++ app/components/ui/NewChatText.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-full flex justify-center items-center w-full">
-        <div class="text-center w-[65%]">
+        <div class="text-center w-full sm:w-[65%]">
             <span style="font-weight: 500;font-size: 24px;">What's on your schedule today?</span>
             <slot></slot>
         </div>
