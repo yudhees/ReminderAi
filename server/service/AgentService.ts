a1ff95491d4391c2f0b50aee1f112898bba74db6
Yudhees <yudees479@gmail.com>
2026-08-09 20:50:19 +0530

    Add agent memory

--- server/service/AgentService.ts
+++ server/service/AgentService.ts
@@ -2,22 +2,29 @@
 import { ChatGroq } from "@langchain/groq"
 import { createAgent, providerStrategy, tool } from "langchain";
 import PromptService from "./PromptService";
+import { MongoDBSaver } from "@langchain/langgraph-checkpoint-mongodb";
+import { MongoClient  } from "mongodb";
 
 export default class AgentService extends PromptService {
     protected chatModel: ChatGroq;
+    protected checkpointer: MongoDBSaver;
     constructor() {
         super()
         this.chatModel = new ChatGroq({
             apiKey: process.env.GROQ_API_KEY,
             model: "openai/gpt-oss-120b",
         });
+        const client = new MongoClient(process.env.NUXT_MONGOOSE_URI as string);
+        this.checkpointer = new MongoDBSaver({ client,dbName:"ReminderAI"});
+
     }
     protected getAgent() {
         const agent = createAgent({
             model: this.chatModel,
             tools: [],
             systemPrompt: this.system_interactions,
-            responseFormat: providerStrategy(this.getZodOutputSchema())
+            responseFormat: providerStrategy(this.getZodOutputSchema()),
+            checkpointer:this.checkpointer,
         })
         return agent
     }
@@ -25,9 +32,9 @@ export default class AgentService extends PromptService {
         try {
 
             const agent = this.getAgent()
-            const res = await agent.invoke({ messages: [{ role: "human", content: message }] }, { configurable: { thread_id, user_id } })
+            const res = await agent.invoke({ messages: [{ role: "user", content: message }] }, { configurable: { thread_id, user_id } })
             const last = res.messages.at(-1);
-            if(last?.content){
+            if (last?.content) {
                 const content = last.content
                 return JSON.parse(content) as ChatResponse
             }
